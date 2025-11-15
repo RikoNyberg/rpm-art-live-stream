6b4e8b60936be274023c459fab1282556aab5e1c
Riko Nyberg <riko.nyberg@adalyon.com>
2025-11-16 00:22:23 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from visualization import visualize_dat
 
 # Display with 100 ms windows and RPM estimation
-visualize_dat("data/drone_idle.dat", window_ms=100, rpm_range=(1000, 7000))
+visualize_dat(
+    # "data/fan_const_rpm.dat",
+    "data/drone_moving.dat",
+    window_ms=10,
+    rpm_range=(1000, 70000),
+    force_speed=True,
+    # min_cluster_size=50,
+    # display=True,
+)
