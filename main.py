87c458045c27d3e434a27c0bb0253d7296360b66
Riko Nyberg <riko.nyberg@adalyon.com>
2025-11-16 02:55:56 +0200

    setup how to call the data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterator
+from typing import Iterator, Literal
 
 from evio.core.pacer import Pacer
 from evio.core.recording import open_dat
@@ -27,7 +27,9 @@ def stream_dat_file(
     recording = open_dat(dat_path, width=width, height=height)
     width = int(recording.width)
     height = int(recording.height)
-    src = DatFileSource(dat_path, window_length_us=window_us, width=width, height=height)
+    src = DatFileSource(
+        dat_path, window_length_us=window_us, width=width, height=height
+    )
     pacer = Pacer(speed=speed, force_speed=force_speed)
 
     for batch_range in pacer.pace(src.ranges()):
@@ -38,8 +40,14 @@ def stream_dat_file(
             batch_range.stop,
         )
         ts_us = recording.timestamps[batch_range.start : batch_range.stop]
-        start_ts_us = int(ts_us[0]) if ts_us.size else int(getattr(batch_range, "start_ts_us", 0))
-        end_ts_us = int(ts_us[-1]) if ts_us.size else int(getattr(batch_range, "end_ts_us", start_ts_us))
+        start_ts_us = (
+            int(ts_us[0]) if ts_us.size else int(getattr(batch_range, "start_ts_us", 0))
+        )
+        end_ts_us = (
+            int(ts_us[-1])
+            if ts_us.size
+            else int(getattr(batch_range, "end_ts_us", start_ts_us))
+        )
         yield EventWindow(
             x_coords=x_coords,
             y_coords=y_coords,
@@ -53,16 +61,39 @@ def stream_dat_file(
         )
 
 
+configs = {
+    "fan_const_rpm": {
+        "dat_path": "data/fan_const_rpm.dat",
+        "blade_count": 3,
+        "window_ms": 100,
+        "min_cluster_size": 50,
+    },
+    "drone_moving": {
+        "dat_path": "data/drone_moving.dat",
+        "blade_count": 2,
+        "window_ms": 10,
+        "min_cluster_size": 50,
+    },
+    "fred_events": {
+        "dat_path": "data/fred_events.dat",
+        "blade_count": 2,
+        "window_ms": 10,
+        "min_cluster_size": 10,
+    },
+}
+
 if __name__ == "__main__":
+    config: Literal["fan_const_rpm", "drone_moving", "fred_events"] = "drone_moving"
     dat_stream = stream_dat_file(
-        "data/drone_moving.dat",
-        window_ms=10.0,
+        dat_path=configs[config]["dat_path"],
+        window_ms=configs[config]["window_ms"],
         speed=1.0,
         force_speed=True,
     )
     visualize_dat(
         dat_stream,
-        blade_count=2,
+        blade_count=configs[config]["blade_count"],
         rpm_range=(1000, 70000),
         min_fill_ratio=0.15,
+        min_cluster_size=configs[config]["min_cluster_size"],
     )
